fa6ef4df024b2e6ac186df0193f3e451eb98de2a
Thalpy <33956696+Thalpy@users.noreply.github.com>
2025-02-04 19:55:50 +0000

    Redirects work, 404 works

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const urlParams = new URLSearchParams(window.location.search);
 const redirectPath = urlParams.get("redirect");
 
 if (redirectPath) {
-    window.history.replaceState({}, "", redirectPath); // Clean the URL
+    window.history.replaceState({}, "", redirectPath);
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
